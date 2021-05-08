b5a97cbba001d8ea4563a6a4c4643dcf27ac3609
MikeDuz <mike1978@list.ru>
2021-05-08 18:55:11 +0300

    08.05.2021 HTML troubles

--- src/main/java/skillbox/controller/DefaultController.java
+++ src/main/java/skillbox/controller/DefaultController.java
@@ -1,17 +1,18 @@
 package skillbox.controller;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController //определяем класс как контроллер
-@RequestMapping("/")// задаем URL
+@Controller //определяем класс как контроллер
 @Log4j2
 public class DefaultController {
 
     @RequestMapping("/")
     public String index() {
-       return "";
+        return "index";
     }
 
 }
